7685558a4fc678d4839da509e9fb626d8f090360
nuttapatgd <nuttapat.gd@gmail.com>
2025-09-01 18:00:41 +0700

    update cors-V2

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://rag-notes-black.vercel.app/",
+    "https://rag-notes-black.vercel.app"
   ], // frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };
